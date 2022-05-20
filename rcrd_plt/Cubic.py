83f8f9455d42297b6210bda2f7127b548e57b409
Mikko De Torres <93197249+MGundam@users.noreply.github.com>
2022-05-20 14:29:35 +0800

    Update Cubic.py

--- rcrd_plt/Cubic.py
+++ rcrd_plt/Cubic.py
@@ -20,3 +20,18 @@ vf = deg_to_rad(vf)
 ti = float(input('ti = ')) # initial time
 tf = float(input('tf = ')) # final time
 
+# Cubic
+# Solve the solution for q(t) = a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3
+
+x = np.arange(ti,tf,0.05)
+
+def cubic(t,a,b,c):
+    return a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3
+
+y = cubic(x,qi,qf,tf)
+
+plt.figure()
+plt.plot(x,y,'g',linestyle='-')
+plt.text(1,1.5,'q(t) = a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3')
+plt.grid(True)
+plt.show()
