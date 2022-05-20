3ff88476d33e35680c796e516c9069f90296450f
Mikko De Torres <93197249+MGundam@users.noreply.github.com>
2022-05-20 13:48:08 +0800

    Added Project Folder
    1. Cubic Polynomial for Revolute
    2. Cubic Polynomial for Prismatic
    3. Quintic Polynomial for Revolute

---
+++ rcrd_plt/Cube_P.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# degrees to radian converter
+def mm_to_m(d):
+    m=1000
+    return d/m
+
+qi = float(input('qi = ')) # initial position
+qi = mm_to_m(qi)
+
+vi = float(input('vi = ')) # initial velocity
+vi = mm_to_m(vi)
+
+qf = float(input('qf = ')) # final position
+qf = mm_to_m(qf)
+
+vf = float(input('vf = ')) # final velocity
+vf = mm_to_m(vf)
+
+ti = float(input('ti = ')) # initial time
+tf = float(input('tf = ')) # final time
+
+# Cubic
+# Solve the solution for q(t) = a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3
+
+x = np.arange(ti,tf,0.05)
+
+def cubic(t,a,b,c):
+    return a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3
+
+y = cubic(x,qi,qf,tf)
+
+plt.figure()
+plt.plot(x,y,'r',linestyle='-')
+plt.text(1,1.5,'q(t) = a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3')
+plt.grid(True)
+plt.show()
---
+++ rcrd_plt/Cubic.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# degrees to radian converter
+def deg_to_rad(T):
+    return (T/180)*np.pi
+
+qi = float(input('qi = ')) # initial position
+qi = deg_to_rad(qi)
+
+vi = float(input('vi = ')) # initial velocity
+vi = deg_to_rad(vi)
+
+qf = float(input('qf = ')) # final position
+qf = deg_to_rad(qf)
+
+vf = float(input('vf = ')) # final velocity
+vf = deg_to_rad(vf)
+
+ti = float(input('ti = ')) # initial time
+tf = float(input('tf = ')) # final time
+
+# Cubic
+# Solve the solution for q(t) = a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3
+
+x = np.arange(ti,tf,0.05)
+
+def cubic(t,a,b,c):
+    return a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3
+
+y = cubic(x,qi,qf,tf)
+
+plt.figure()
+plt.plot(x,y,'g',linestyle='-')
+plt.text(1,1.5,'q(t) = a + (3*(b-a)/c**2)*t**2 - (2*(b-a)/c**3)*t**3')
+plt.grid(True)
+plt.show()
